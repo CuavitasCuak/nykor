4806b39b239b67ab4e3afd2da0c29625fec7593b
CuavitasCuak <marincuaava@gmail.com>
2026-06-30 16:58:56 -0500

    actualizando App.jsx

--- Nykor/src/App.jsx
+++ Nykor/src/App.jsx
@@ -1,121 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Routes, Route } from "react-router-dom"
+import { Login } from "./assets/Componentes/Login";
+import { Home } from "./assets/Componentes/Home"
+import { Comic } from "./assets/Componentes/Comic"
+import { Tienda } from "./assets/Componentes/Tienda"
+import { Prologo } from "./assets/Componentes/Prologo"
+import { Capitulo1 } from "./assets/Componentes/Capitulo1"
+import { Capitulo2 } from "./assets/Componentes/Capitulo2"
+import { Capitulo3 } from "./assets/Componentes/Capitulo3"
+import { Capitulo4 } from "./assets/Componentes/Capitulo4"
+import { Capitulo5 } from "./assets/Componentes/Capitulo5"
+import { Capitulo6 } from "./assets/Componentes/Capitulo6"
+import { Capitulo7 } from "./assets/Componentes/Capitulo7"
+import { Capitulo8 } from "./assets/Componentes/Capitulo8"
+import { Capitulo9 } from "./assets/Componentes/Capitulo9"
+import { Capitulo10 } from "./assets/Componentes/Capitulo10"
+import { Capitulo11 } from "./assets/Componentes/Capitulo11"
+import { Capitulo12 } from "./assets/Componentes/Capitulo12"
+import { Capitulo13 } from "./assets/Componentes/Capitulo13"
+import { Capitulo14 } from "./assets/Componentes/Capitulo14"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <Routes>
+      {/* HOME */}
+      <Route path="/" element={<Home />} />
 
-      <div className="ticks"></div>
+      {/* SECCIONES */}
+      <Route path="/comic" element={<Comic />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/tienda" element={<Tienda />} />
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      {/* CAPÍTULOS */}
+      <Route path="/comic/Prologo" element={<Prologo />} />
+      <Route path="/comic/capitulo1" element={<Capitulo1 />} />
+      <Route path="/comic/capitulo2" element={<Capitulo2 />} />
+      <Route path="/comic/capitulo3" element={<Capitulo3 />} />
+      <Route path="/comic/capitulo4" element={<Capitulo4 />} />
+      <Route path="/comic/capitulo5" element={<Capitulo5 />} />
+      <Route path="/comic/capitulo6" element={<Capitulo6 />} />
+      <Route path="/comic/capitulo7" element={<Capitulo7 />} />
+      <Route path="/comic/capitulo8" element={<Capitulo8 />} />
+      <Route path="/comic/capitulo9" element={<Capitulo9 />} />
+      <Route path="/comic/capitulo10" element={<Capitulo10 />} />
+      <Route path="/comic/capitulo11" element={<Capitulo11 />} />
+      <Route path="/comic/capitulo12" element={<Capitulo12 />} />
+      <Route path="/comic/capitulo13" element={<Capitulo13 />} />
+      <Route path="/comic/capitulo14" element={<Capitulo14 />} />
+    </Routes>
   )
 }
 
